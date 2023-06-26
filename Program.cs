73378d2877018046ca6c838a3022886e65e99918
Даниил Коротков <dkorotkov@cleverence.ru>
2023-06-26 15:58:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using AdvancedSharpAdbClient;
+
+namespace AdvancedSharpAdbClientTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var client = new AdbClient();
+            client.Connect("127.0.0.1:62001");
+            var device = client.GetDevices().FirstOrDefault(); // Get first connected device
+        }
+    }
+}
